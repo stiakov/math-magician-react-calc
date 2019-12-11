b19141ebdd1e86c98d8500738b10ba8ebeb2b4b1
Santiago Torres G <thdealer@gmail.com>
2019-12-11 17:16:29 -0500

    fixing lint error for class, updating to className

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,31 +5,31 @@ export class ButtonPanel extends React.Component {
   render() {
     return (
       <div id='panel'>
-        <div class='row'>
+        <div className='row'>
           <Button name='AC' />
           <Button name='+/-' />
           <Button name='%' />
           <Button name='/' color={ true } />
         </div>
-        <div class='row'>
+        <div className='row'>
           <Button name='7' />
           <Button name='8' />
           <Button name='9' />
           <Button name='x' color={ true } />
         </div>
-        <div class='row'>
+        <div className='row'>
           <Button name='4' />
           <Button name='5' />
           <Button name='6' />
           <Button name='–' color={ true } />
         </div>
-        <div class='row'>
+        <div className='row'>
           <Button name='1' />
           <Button name='2' />
           <Button name='3' />
           <Button name='+' color={ true } />
         </div>
-        <div class='row'>
+        <div className='row'>
           <Button name='0' wide={ true } />
           <Button name='.' />
           <Button name='=' color={ true }/>
