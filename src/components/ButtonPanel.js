efc8c1e6e97630c8dc11de1eea713867b2c4eb78
Santiago Torres G <thdealer@gmail.com>
2019-12-11 12:50:33 -0500

    ButtonPanel comp added

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,1 +1,40 @@
 import React from 'react';
+import { Button } from './Button';
+
+export class ButtonPanel extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Button id='AC' />
+          <Button id='+/-' />
+          <Button id='%' />
+          <Button id='/' />
+        </div>
+        <div>
+          <Button id='7' />
+          <Button id='8' />
+          <Button id='9' />
+          <Button id='X' />
+        </div>
+        <div>
+          <Button id='4' />
+          <Button id='5' />
+          <Button id='6' />
+          <Button id='–' />
+        </div>
+        <div>
+          <Button id='1' />
+          <Button id='2' />
+          <Button id='3' />
+          <Button id='+' />
+        </div>
+        <div>
+          <Button id='.' />
+          <Button id='0' />
+          <Button id='=' />
+        </div>
+      </div>
+    )
+  }
+}
