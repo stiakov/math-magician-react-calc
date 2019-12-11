37e0464c58c05b65c9dd21f3588ac32f281ddc5b
Santiago Torres G <thdealer@gmail.com>
2019-12-11 12:51:03 -0500

    Display comp added

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,1 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Display =({ result }) => <div>{ result }</div>;
+
+Display.propTypes = { result : PropTypes.string };
+Display.defaultProps = { result: '0' };
