9275441e2bbac9faae5dfb5a85eff8ee4eec0018
Santiago Torres G <thdealer@gmail.com>
2019-12-16 18:38:07 -0500

    adding click handler

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,19 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 const orange = '#f6913f';
 
-export const Button = ({ name, color, wide }) => {
+export const Button = ({ name, color, wide, clickHandler }) => {
   const styles = {
     backgroundColor: color,
     width: wide ? '50%' : '25%',
     height: '100px',
   }
-  return <button style={ styles } >{ name }</button>
+  return <button onClick={() => clickHandler(name)} style={ styles } >
+    { name }
+    </button>
 };
 
 Button.propTypes = { 
   name: PropTypes.string.isRequired,
   wide: PropTypes.bool,
   color: PropTypes.string,
+  clickHandler: PropTypes.func
  };
 
  Button.defaultProps = { wide: false, color: orange };
