fd9a37b901996ec9ecaf8bf9e5dfde0de50ab1bc
Santiago Torres G <thdealer@gmail.com>
2019-12-16 18:34:52 -0500

    updating proptypes for clickHandler

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,5 +3,5 @@ import PropTypes from 'prop-types';
 
 export const Display =({ result }) => <div id='display'>{ result }</div>;
 
-Display.propTypes = { result : PropTypes.string };
+Display.propTypes = { result : PropTypes.string.isRequired };
 Display.defaultProps = { result: '0' };
